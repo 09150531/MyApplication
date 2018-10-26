c3acf8764e524e72484089289e6b8115ca4161b4
09150531 <tangxm1314@sina.com>
2018-10-26 11:11:39 +0800

    2

---
+++ Lesson9/src/main/java/com/example/myapplication/AddActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.add);
+    }
+}
